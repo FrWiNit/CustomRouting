d7690004970338484cd23574d260e35eb8eae87b
Alex <raevsky.ref@gmail.com>
2026-02-18 02:22:01 +0200

    [Feature] Public added

--- Sources/CustomRouting/Core/RouterView.swift
+++ Sources/CustomRouting/Core/RouterView.swift
@@ -7,7 +7,7 @@
 import SwiftUI
 
 @MainActor
-struct RouterView<Content: View>: View, @MainActor Router {
+public struct RouterView<Content: View>: View, @MainActor Router {
     @Environment(\.dismiss) var dismiss
     @State private var path: [AnyDestination] = []
     @State private var showSheet: AnyDestination? = nil
@@ -33,7 +33,7 @@ struct RouterView<Content: View>: View, @MainActor Router {
         self.content = content
     }
     
-    var body: some View {
+    public var body: some View {
         NavigationstackIfNeeded(path: $path, addNavigationView: addNavigationView) {
             content(self)
                 .sheetViewModifier(screen: $showSheet)
@@ -44,7 +44,7 @@ struct RouterView<Content: View>: View, @MainActor Router {
         .environment(\.router, self)
     }
     
-    func showScreen<T: View>(_ option: SegueOption, @ViewBuilder destination: @escaping (Router) -> T) {
+    public func showScreen<T: View>(_ option: SegueOption, @ViewBuilder destination: @escaping (Router) -> T) {
         let screen = RouterView<T>(
             screenStack: option.shouldAddNewNavigationView ? nil : (screenStack.isEmpty ? $path : $screenStack),
             addNavigationView: option.shouldAddNewNavigationView
@@ -68,27 +68,27 @@ struct RouterView<Content: View>: View, @MainActor Router {
         }
     }
     
-    func dismissScreen() {
+    public func dismissScreen() {
         dismiss()
     }
     
-    func showAlert(_ option: AlertType, title: String, subtitle: String? = nil, buttons: (@Sendable () -> AnyView)? = nil) {
+    public func showAlert(_ option: AlertType, title: String, subtitle: String? = nil, buttons: (@Sendable () -> AnyView)? = nil) {
         self.alertOption = option
         self.alert = AnyAppAlert(title: title, subtitle: subtitle, buttons: buttons)
     }
     
-    func dismissAlert() {
+    public func dismissAlert() {
         alert = nil
     }
     
-    func showModal<T: View>(backgroundColor: Color, transition: AnyTransition, @ViewBuilder destination: @escaping () -> T) {
+    public func showModal<T: View>(backgroundColor: Color, transition: AnyTransition, @ViewBuilder destination: @escaping () -> T) {
         self.modalBackgroundColor = backgroundColor
         self.modalTransition = transition
         let destination = AnyDestination(destination: destination())
         self.modal = destination
     }
     
-    func dismissModal() {
+    public func dismissModal() {
         modal = nil
     }
 }
